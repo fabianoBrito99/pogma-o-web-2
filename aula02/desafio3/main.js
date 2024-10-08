53121635644404b12a01dd26042dd5b32c00834e
Fabiano Brito <fabiano99.fbs@gmail.com>
2024-10-08 18:12:17 -0300

    ajuste com if para verificar a situação do aluno

--- aula02/desafio3/main.js
+++ aula02/desafio3/main.js
@@ -1,16 +1,22 @@
 // 3. Vetor de alunos com nome, nota1, nota2, média e exibição das informações
 let alunos = [
   { nome: 'João', nota1: 7.5, nota2: 8.0 },
-  { nome: 'Maria', nota1: 9.0, nota2: 7.5 },
+  { nome: 'Maria', nota1: 7.0, nota2: 7.0 },
   { nome: 'Carlos', nota1: 6.0, nota2: 5.5 }
 ];
 
 for (let i = 0; i < alunos.length; i++) {
   let media = (alunos[i].nota1 + alunos[i].nota2) / 2;
-  console.log("Nome: " + alunos[i].nome + " - Média: " + media.toFixed(2));
+
+  if (media > 7.0) {
+      console.log("Nome: " + alunos[i].nome + " - Média: " + media.toFixed(2) + " - Aluno Aprovado");
+  } else if (media === 7.0) {
+      console.log("Nome: " + alunos[i].nome + " - Média: " + media.toFixed(2) + " - Aluno em Recuperação");
+  } else {
+      console.log("Nome: " + alunos[i].nome + " - Média: " + media.toFixed(2) + " - Aluno Reprovado");
+  }
 }
 
-// Cálculo da média geral dos alunos
 let somaMedias = 0;
 for (let i = 0; i < alunos.length; i++) {
   somaMedias += (alunos[i].nota1 + alunos[i].nota2) / 2;
